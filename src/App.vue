d9db4fa9e5287511dbc344673744858a31f214f7
kaijie26 <chenkaijie26@gmail.com>
2022-07-08 17:28:05 +0200

    create components files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSpotify/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderSpotify from './components/HeaderSpotify.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderSpotify,
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/common';
+
 </style>
---
+++ src/components/HeaderSpotify.vue
@@ -0,0 +1,20 @@
+<!-- HTML -->
+<template>
+    <header>
+        
+    </header>
+  
+</template>
+
+<!-- JAVASCRIPT -->
+<script>
+export default {
+
+    
+}
+</script>
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+</style>
